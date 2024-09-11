57cc34fb2e3094b51adbace39b612a81f1a12365
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-09-11 15:51:23 +0300

    firebase auth signout and alerts

--- src/services/firebase.auth.js
+++ src/services/firebase.auth.js
@@ -1,10 +1,13 @@
 import Swal from "sweetalert2";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  // onAuthStateChanged,
+  updateProfile,
 } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,24 +25,24 @@ const firebaseConfig = {
 // Initialize Firebase
 // eslint-disable-next-line no-unused-vars
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+// let user = auth.currentUser;
 
 // login function
 export async function signIn(email, password) {
-  const auth = getAuth();
   const res = signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
+    .then(async (userCredential) => {
+      // logged in
       const user = userCredential.user;
       Swal.fire({
-        title: "Welcome Back!",
+        title: "Logged In Successfully!",
         icon: "success",
         showConfirmButton: false,
         timer: 2000,
       });
-      return user;
+      return user.toJSON();
     })
     .catch((error) => {
-      const errorCode = error.code;
       const errorMessage = error.message;
       Swal.fire({
         title: `Error!`,
@@ -47,26 +50,26 @@ export async function signIn(email, password) {
         icon: "error",
         confirmButtonText: "OK",
       });
-      console.log("error code: ", errorCode);
-      console.log("error message: ", errorMessage);
     });
+
   return res;
 }
 
 // sign up
-export async function signUp(email, password) {
-  const auth = getAuth();
+export async function signUp(email, password, fullName) {
   const res = createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
+    .then(async (userCredential) => {
       // Signed up
       const user = userCredential.user;
+      await updateProfile(user, { displayName: fullName });
+
       Swal.fire({
         title: "Account Created Successfully!",
         icon: "success",
         showConfirmButton: false,
         timer: 2000,
       });
-      return user;
+      return user.toJSON();
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -82,3 +85,34 @@ export async function signUp(email, password) {
     });
   return res;
 }
+
+export async function signOut() {
+  auth
+    .signOut()
+    .then(() => {
+      Swal.fire({
+        title: "Logged Out Successfully!",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      Swal.fire({
+        title: `Error!`,
+        text: errorMessage,
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+      console.log("error code: ", errorCode);
+      console.log("error message: ", errorMessage);
+    });
+}
+
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
+//     console.log(user.toJSON());
+//   } else console.log(user);
+// });
