2b4cabd7e38c0963305a3c43c9cf95fed1400466
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-09-18 00:29:13 +0300

    made into a modal

--- src/components/Shared/Loading.jsx
+++ src/components/Shared/Loading.jsx
@@ -1,9 +1,9 @@
 function Loading() {
   return (
     <>
-      <div className="min-vw-100 min-vh-100">
-        <div className="d-flex justify-content-center ">
-          <div className="spinner-border" role="status">
+      <div className="modal d-block bg-opacity-50 bg-secondary h-100">
+        <div className="modal-dialog modal-dialog-centered justify-content-center">
+          <div className="spinner-border " role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
         </div>
